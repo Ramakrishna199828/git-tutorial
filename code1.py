82dc7375564ef1714452e1d504d8b3db79ed4d78
Ramakrishna199828 <rknayudu1@gmail.com>
2025-02-12 17:24:37 +0530

    Chanbges made on code1.py file

--- code1.py
+++ code1.py
@@ -1,1 +1,15 @@
 print("Welcome to Git Learning!!")
+
+
+print("Hello World")
+
+a = 10
+b = 20
+c = a + b
+print("-"*50)
+print("-"*50)
+print("The Sum of {},{} = {}".format(a,b,c))
+print("-"*50)
+print("-"*50)
+
+
