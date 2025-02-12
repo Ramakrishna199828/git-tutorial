f6aba823028d95f9e4913284cc8ac4707e218417
Ramakrishna199828 <rknayudu1@gmail.com>
2025-02-12 17:33:26 +0530

    Modifying the code1.py

--- code1.py
+++ code1.py
@@ -6,10 +6,13 @@ print("Hello World")
 a = 10
 b = 20
 c = a + b
-print("-"*50)
+print("%"*50)
 print("-"*50)
 print("The Sum of {},{} = {}".format(a,b,c))
 print("-"*50)
-print("-"*50)
+print("%"*50)
+
+
+
 
 
